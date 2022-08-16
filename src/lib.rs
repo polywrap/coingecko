1d33175325baeecf4a3d1f92837ddf0a589eb94e
cbrzn <cesarbrazon10@gmail.com>
2022-08-16 19:01:07 +0200

    chore: derivatives exchange list added

--- src/lib.rs
+++ src/lib.rs
@@ -1180,44 +1180,44 @@ pub fn ping(_args: ArgsPing) -> Ping {
 // }
 
 
-// pub fn derivativesExchangesList(args: ArgsDerivativesExchangesList) -> DerivativesExchangesList {
-//     let url_params: Option<Vec<HttpUrlParam>> = Some(vec![
+pub fn derivatives_exchanges_list(args: ArgsDerivativesExchangesList) -> Vec<DerivativeExchangeId> {
+    let url_params: Option<Vec<HttpUrlParam>> = Some(vec![
 
-//     ]);
-//     let http_response: HttpResponse = match HttpModule::get(&http_module::ArgsGet {
-//         url: "https://api.coingecko.com/api/v3/derivatives/exchanges/list".to_string(),
-//         request: Some(HttpRequest {
-//             headers: None,
-//             url_params: url_params,
-//             response_type: HttpResponseType::TEXT,
-//             body: None,
-//         }),
-//     }) {
-//         Ok(Some(v)) => v,
-//         Ok(None) => panic!("Did not receive HTTP response"),
-//         Err(e) => panic!("{}", e),
-//     };
+    ]);
+    let http_response: HttpResponse = match HttpModule::get(&http_module::ArgsGet {
+        url: "https://api.coingecko.com/api/v3/derivatives/exchanges/list".to_string(),
+        request: Some(HttpRequest {
+            headers: None,
+            url_params: url_params,
+            response_type: HttpResponseType::TEXT,
+            body: None,
+        }),
+    }) {
+        Ok(Some(v)) => v,
+        Ok(None) => panic!("Did not receive HTTP response"),
+        Err(e) => panic!("{}", e),
+    };
 
-//     // handle json rpc error
-//     if http_response.status == 400 || http_response.status == 404 || http_response.status == 500 {
-//         let response_body: String = match http_response.body {
-//             Some(v) => v,
-//             None => "An unknown error occurred!".to_string()
-//         };
+    // handle json rpc error
+    if http_response.status == 400 || http_response.status == 404 || http_response.status == 500 {
+        let response_body: String = match http_response.body {
+            Some(v) => v,
+            None => "An unknown error occurred!".to_string()
+        };
 
-//         panic!("Error {}: {}", http_response.status, response_body)
-//     }
+        panic!("Error {}: {}", http_response.status, response_body)
+    }
 
-//     // handle json rpc success
-//     if http_response.status >= 200 && http_response.status <= 299 {
-//         return match http_response.body {
-//             Some(v) => JSON::from_str::<DerivativesExchangesList>(&v).unwrap(),
-//             None => panic!("Missing response with successful HTTP status {}", http_response.status)
-//         };
-//     }
+    // handle json rpc success
+    if http_response.status >= 200 && http_response.status <= 299 {
+        return match http_response.body {
+            Some(v) => JSON::from_str::<Vec<DerivativeExchangeId>>(&v).unwrap(),
+            None => panic!("Missing response with successful HTTP status {}", http_response.status)
+        };
+    }
 
-//     panic!("Unexpected HTTP response with status: {}", http_response.status);
-// }
+    panic!("Unexpected HTTP response with status: {}", http_response.status);
+}
 
 
 // pub fn exchangesVolumeChart(args: ArgsExchangesVolumeChart) -> ExchangesVolumeChart {

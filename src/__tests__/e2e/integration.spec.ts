5c96ce4c52265cffe56b9705fefafa65007c88d3
Niraj Kamdar <nirajkamdar27@pm.me>
2023-09-26 17:27:04 +0400

    chore: bump polywrap version

--- src/__tests__/e2e/integration.spec.ts
+++ src/__tests__/e2e/integration.spec.ts
@@ -1,6 +1,6 @@
 import { PolywrapClient } from "@polywrap/client-js";
 import path from "path";
-import { Coingecko_Module } from "../types";
+import { Coingecko } from "../types";
 
 jest.setTimeout(500000);
 
@@ -14,44 +14,70 @@ describe("Coingecko", () => {
   const wrapperUri = `fs/${wrapperPath}/build`;
 
   const client: PolywrapClient = new PolywrapClient();
+  const gecko = new Coingecko(client, undefined, wrapperUri);
 
   test("ping", async () => {
-    const ping = await Coingecko_Module.ping({}, client, wrapperUri);
-    expect(ping.error).toBeFalsy();
-    expect(ping.data).toBeTruthy();
-    expect(ping.data?.gecko_says).toStrictEqual("(V3) To the Moon!");
+    const ping = await gecko.ping({});
+    if (!ping.ok) {
+      expect(ping.error).toBeFalsy();
+      throw new Error("ping error");
+    }
+    expect(ping.value?.gecko_says).toStrictEqual("(V3) To the Moon!");
   });
 
   test("simplePrice", async () => {
-    const simplePrice = await Coingecko_Module.simplePrice({ids: "bitcoin", vs_currencies: "usd"}, client, wrapperUri);
-    expect(simplePrice.error).toBeFalsy();
-    expect(simplePrice.data).toBeTruthy();
+    const simplePrice = await gecko.simplePrice({
+      ids: "bitcoin",
+      vs_currencies: "usd",
+    });
+    expect(simplePrice.ok).toBeTruthy();
+    if (!simplePrice.ok) {
+      expect(simplePrice.error).toBeFalsy();
+      throw new Error("simplePrice error");
+    }
+    expect(simplePrice.value).toBeTruthy();
   });
 
   test("simpleTokenPrice", async () => {
-    const simplePrice = await Coingecko_Module.simpleTokenPrice({id: "ethereum", contract_addresses: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF", vs_currencies: "usd"}, client, wrapperUri);
-    expect(simplePrice.error).toBeFalsy();
-    expect(simplePrice.data).toBeTruthy();
+    const simpleTokenPrice = await gecko.simpleTokenPrice({
+      id: "ethereum",
+      contract_addresses: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+      vs_currencies: "usd",
+    });
+    if (!simpleTokenPrice.ok) {
+      expect(simpleTokenPrice.error).toBeFalsy();
+      throw new Error("simpleTokenPrice error");
+    }
+    expect(simpleTokenPrice.value).toBeTruthy();
   })
 
   test("simpleSupportedVsCurrencies", async () => {
-    const simpleSupportedVsCurrencies = await Coingecko_Module.simpleSupportedVsCurrencies({}, client, wrapperUri);
-    expect(simpleSupportedVsCurrencies.error).toBeFalsy();
-    expect(simpleSupportedVsCurrencies.data).toBeTruthy();
-    expect(simpleSupportedVsCurrencies.data?.length).toBeGreaterThan(0);
+    const simpleSupportedVsCurrencies = await gecko.simpleSupportedVsCurrencies({});
+    if (!simpleSupportedVsCurrencies.ok) {
+      expect(simpleSupportedVsCurrencies.error).toBeFalsy();
+      throw new Error("simpleSupportedVsCurrencies error");
+    }
+    expect(simpleSupportedVsCurrencies.value).toBeTruthy();
+    expect(simpleSupportedVsCurrencies.value?.length).toBeGreaterThan(0);
   });
 
   test("coinsList", async () => {
-    const coinsList = await Coingecko_Module.coinsList({include_platform: true}, client, wrapperUri);
-    expect(coinsList.error).toBeFalsy();
-    expect(coinsList.data).toBeTruthy();
-    expect(coinsList.data?.length).toBeGreaterThan(0);
+    const coinsList = await gecko.coinsList({include_platform: true});
+    if (!coinsList.ok) {
+      expect(coinsList.error).toBeFalsy();
+      throw new Error("coinsList error");
+    }
+    expect(coinsList.value).toBeTruthy();
+    expect(coinsList.value?.length).toBeGreaterThan(0);
   });
 
   test("coinMarkets", async () => {
-    const coinMarkets = await Coingecko_Module.coinsMarkets({vs_currency: "usd", ids: "ethereum"}, client, wrapperUri);
-    expect(coinMarkets.error).toBeFalsy();
-    expect(coinMarkets.data).toBeTruthy();
-    expect(coinMarkets.data?.length).toBeGreaterThan(0);
+    const coinMarkets = await gecko.coinsMarkets({vs_currency: "usd", ids: "ethereum"});
+    if (!coinMarkets.ok) {
+      expect(coinMarkets.error).toBeFalsy();
+      throw new Error("coinMarkets error");
+    }
+    expect(coinMarkets.value).toBeTruthy();
+    expect(coinMarkets.value?.length).toBeGreaterThan(0);
   });
 });
